4e59fccabc3778d7938d72c4cd2a2a82e68db115
lmMoulis <leon.moulis@gmail.com>
2026-09-12 16:06:10 +0200

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Demo from './pages/Demo.jsx';
 import Admin from './pages/Admin.jsx';
+import Event from './pages/Event.jsx';
 
 function Home() {
   return (
@@ -480,6 +481,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/demo" element={<Demo />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/e/:slug" element={<Event />} />
     </Routes>
   );
 }
